feb7ef149a73d2c6a9e09dc4ab9a4301b904a558
Eugene <setminsk@gmail.com>
2022-06-07 14:18:40 +0300

    Added logging

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import lombok.extern.log4j.Log4j2;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,9 +13,7 @@ public class LoginTest extends BaseTest {
         loginPage.open();
         loginPage.login("setminsk@gmail.com","100200300");
         assertEquals(loginPage.title(),"Мой аккаунт - http://prestashop.qatestlab.com.ua/");
-
     }
-
     @Test
     public void invalidEmailPassword() {
         loginPage.open();
@@ -26,28 +25,23 @@ public class LoginTest extends BaseTest {
         loginPage.open();
         loginPage.login("","");
         assertEquals(loginPage.getError1(),"An email address required.");
-
     }
     @Test
     public void cleanPassword() {
         loginPage.open();
         loginPage.login("setminsk@gmail.com","");
         assertEquals(loginPage.getError2(),"Password is required.");
-
     }
-
     @Test
     public void wrongPassword() {
         loginPage.open();
         loginPage.login("setminsk@gmail.com","55555");
         assertEquals(loginPage.getError3(), "Authentication failed.");
     }
-
     @Test
     public void wrongEmail() {
         loginPage.open();
         loginPage.login("setdssdf@gmail.com","55555");
         assertEquals(loginPage.getError3(), "Authentication failed.");
     }
-
 }
